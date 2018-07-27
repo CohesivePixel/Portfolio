f6459205b34e59e8cd5fd67411562a92cfae8275
Ben-Ey <logins@ben-ey.nl>
2018-07-27 23:52:19 +0200

    transformed icons into links

--- src/components/social-icons.vue
+++ src/components/social-icons.vue
@@ -1,21 +1,26 @@
 <template lang="html">
   <div class="social-container">
-    <img class="social-icon" src="../assets/icons/facebook.svg" />
-    <img class="social-icon" src="../assets/icons/instagram.svg" />
-    <img class="social-icon" src="../assets/icons/twitter.svg" />
-    <img class="social-icon" src="../assets/icons/whatsapp.svg" />
-    <img class="social-icon" src="../assets/icons/youtube.svg" />
+    <a href="#"><img class="social-icon" src="../assets/icons/facebook.svg" /></a>
+    <a href="#"><img class="social-icon" src="../assets/icons/instagram.svg" /></a>
+    <a href="#"><img class="social-icon" src="../assets/icons/twitter.svg" /></a>
+    <a href="#"><img class="social-icon" src="../assets/icons/whatsapp.svg" /></a>
+    <a href="#"><img class="social-icon" src="../assets/icons/youtube.svg" /></a>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      hoverOver: false
+    }
+  }
 }
 </script>
 
 <style lang="scss">
   $size: 3vw;
+  $extended-size: 4vw;
 
   .social-container {
     position: absolute;
@@ -27,5 +32,12 @@ export default {
     width: $size;
     height: $size;
     margin: 0 1.5vw;
+    transition: .3s cubic-bezier(.77,0,.18,1);
+  }
+
+  .sized-icon {
+    @extend .social-icon;
+    width: $extended-size;
+    height: $extended-size;
   }
 </style>
