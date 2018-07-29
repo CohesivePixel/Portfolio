2430b3b5dca1ae49df8b70fb2b7dc73fabfe4fb0
Ben-Ey <logins@ben-ey.nl>
2018-07-29 11:20:10 +0200

    coloured prev button

--- src/components/nav-buttons.vue
+++ src/components/nav-buttons.vue
@@ -1,69 +1,59 @@
 <template lang="html">
   <div class="btn-container">
-    <transition name="btn-prev-slide" v-on:after-leave="changeState">
-        <img class="nav-btn-prev"
-             :src="paths.prevIconPath"
-             @click="dropCounter"
-             @mouseover="setStandard(1)"
-             @moouseleave="setInverted(1)"
-             v-if="showBtn" />
-    </transition>
     <transition name="btn-next-slide" v-on:after-leave="changeState">
-        <img class="nav-btn-next"
-             :src="paths.nextIconPath"
-             @click="addCounter"
-             @mouseover="setStandard(0)"
-             @mouseleave="setInverted(0)"
-             v-if="showBtn" />
+      <!-- <img class="nav-btn" :src="iconPath"  @click="addCounter" @mouseover="setStandard" @mouseleave="setInverted" v-if="showBtn"> -->
+      <svg class="nav-btn-next" x="0px" y="0px" viewBox="0 0 60 60" :style="styles.nextBtnStyle" v-if="showBtn">
+        <g id="Jaartrui---Desktop" transform="translate(-1339.000000, -833.000000)">
+          <path id="Arrow-Right" class="st0" d="M1386.4,881.5l-15.5-15.5c-1-1-2.7-1-3.7,0l-15.5,15.5c-1,1-2.7,1-3.7,0l-7.1-7.1
+            c-1-1-1-2.7,0-3.7l26.3-26.3c1-1,2.7-1,3.7,0l26.3,26.3c1,1,1,2.7,0,3.7l-7.1,7.1C1389.1,882.6,1387.4,882.6,1386.4,881.5z"/>
+        </g>
+      </svg>
+    </transition>
+    <transition name="btn-prev-slide">
+      <svg class="nav-btn-prev" x="0px" y="0px" viewBox="0 0 60 60" :style="prevBtnStyle" v-if="showBtn">
+        <g id="Jaartrui---Desktop" transform="translate(-1339.000000, -833.000000)">
+          <path id="Arrow-Right" class="st0" d="M1386.4,881.5l-15.5-15.5c-1-1-2.7-1-3.7,0l-15.5,15.5c-1,1-2.7,1-3.7,0l-7.1-7.1
+            c-1-1-1-2.7,0-3.7l26.3-26.3c1-1,2.7-1,3.7,0l26.3,26.3c1,1,1,2.7,0,3.7l-7.1,7.1C1389.1,882.6,1387.4,882.6,1386.4,881.5z"/>
+        </g>
+      </svg>
     </transition>
   </div>
 </template>
 
 <script>
 import {common} from 'src/main.js';
-import StandardIcon from 'assets/icons/arrow-standard.svg';
-import InvertedIcon from 'assets/icons/arrow-inverted.svg';
 
 export default {
   data() {
     return {
       shared: common,
       showBtn: true,
-      paths: {
-        nextIconPath: '',
-        prevIconPath: ''
+      styles: {
+        nextBtnStyle: 'fill: white'
       }
     }
   },
 
   created() {
-    this.setInverted(0);
-    this.setInverted(1);
     Event.$on('swipe', () => this.toNext());
   },
 
+  computed: {
+    prevBtnStyle() {
+      return 'fill: ' + this.shared.colour.vibrant;
+    }
+  },
+
   methods: {
-    setStandard(btn) {
-      if(btn) {
-        this.paths.prevIconPath = StandardIcon;
-      } else {
-        this.paths.nextIconPath = StandardIcon;
-      }
-    },
-    setInverted(btn) {
-      if(btn) {
-        this.paths.prevIconPath = InvertedIcon
-      } else {
-        this.paths.nextIconPath = InvertedIcon
-      }
-    },
     dropCounter() {
-      this.shared.active -= 1;
-      this.toNext();
+
     },
     addCounter() {
       this.shared.active += 1;
       this.toNext();
+    },
+    toPrev() {
+
     },
     toNext() {
       this.showBtn = false;
@@ -79,6 +69,7 @@ export default {
 <style lang="scss">
   $size: 2vw;
   $swipe-distance: 10vh;
+  $left-distance: -10vh;
 
   .nav-btn-next {
     width: $size;
@@ -92,9 +83,12 @@ export default {
   .nav-btn-prev {
     @extend .nav-btn-next;
     left: 3vh;
-    fill: red;
+    transform: rotate(-90deg);
   }
 
+  /*
+   * Animation for the 'next' button
+   */
   .btn-next-slide-leave-active {
     transition: all 1.2s ease;
     transform: translateX($swipe-distance)
@@ -109,4 +103,22 @@ export default {
   .btn-next-slide-enter {
     transform: translateX($swipe-distance)
   }
+
+  /*
+   * Animation for the 'previous' button
+   */
+   .btn-prev-slide-leave-active {
+     transition: all 0.8s ease;
+     transform: translateX($left-distance)
+                rotate(180deg);
+   }
+
+   .btn-prev-slide-enter-active {
+     transition: all 0.8s ease;
+     transition-delay: 1.5s;
+   }
+
+   .btn-prev-slide-enter {
+     transform: translateX($left-distance)
+   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// TODO: Create forward- aand backward buttons
 // TODO: Landing page
 // TODO: Adding Content page
 // TODO: Mobile Version
